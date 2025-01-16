7592de23327ed6caafd464355b6ea34503e74541
buihuybaoo <huybaovn9@gmail.com>
2025-01-16 21:54:13 +0700

    Delete Food directory

--- Food/js/script.js
+++
@@ -1,8 +0,0 @@
-function showMenu() {
-    const content = document.getElementById("menu");
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
-}
