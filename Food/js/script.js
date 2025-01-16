62066689e0a5129e33144eee0dbae39b5968fd8e
buihuybaoo <huybaovn9@gmail.com>
2025-01-16 21:48:23 +0700

    Add files via upload

---
+++ Food/js/script.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    const content = document.getElementById("menu");
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
